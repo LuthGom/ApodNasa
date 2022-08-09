c0eab97e050997819bda30ae90f3d151923be5b2
LuthGom <lucianomendes23@yahoo.com>
2022-08-09 20:35:31 -0300

    add evento para tocar a musica quando a pagina carregar

--- js/apodNasa.js
+++ js/apodNasa.js
@@ -1,45 +1,44 @@
 // variável para captar o botão
-const botao = document.querySelector('#btn')
+const botao = document.querySelector("#btn");
 // variavel para captar a div
-const div = document.querySelector('#principal')
+const div = document.querySelector("#principal");
 // variavel para captar o input
-const data = document.querySelector('#data')
+const data = document.querySelector("#data");
 // variavel para captar a main
-const div1 = document.querySelector('#titulo')
+const div1 = document.querySelector("#titulo");
 // variavel para captar a div do copyright
-const div2 = document.querySelector('#copyright')
+const div2 = document.querySelector("#copyright");
 //evento para api aparecer ao apertar no botão
-const sound = document.querySelector('audio')
-window.addEventListener('load', function() {
-    Requisicao(data.value)
-})
+const sound = document.querySelector("audio");
+window.addEventListener("load", function () {
+  Requisicao(data.value);
+  sound.play();
+});
 // evento que carrega a foto do dia, automaticamente, quando a página é carregada
-botao.addEventListener('click', function (e) {
-    e.preventDefault()
-    Requisicao(data.value)
-    sound.play()
-    
-})
+botao.addEventListener("click", function (e) {
+  e.preventDefault();
+  Requisicao(data.value);
+});
 // função que faz a requisição da API APod da Nasa
 async function Requisicao(date) {
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DiuHqrkQtTM0MHOeHeREjXEjMeDOeBRpEoBcdlvr&date=${date}`)
-    console.log(response);
-    let dadosObj = await response.json()
-    console.log(dadosObj);
-    apiNasa(dadosObj)
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=DiuHqrkQtTM0MHOeHeREjXEjMeDOeBRpEoBcdlvr&date=${date}`
+  );
+  console.log(response);
+  let dadosObj = await response.json();
+  console.log(dadosObj);
+  apiNasa(dadosObj);
 }
 // fução que iça os elementos do objeto da APOD requisitados na página
 function apiNasa(dadosObject) {
-    div1.innerHTML = `<h1>${dadosObject.title} (${dadosObject.date})</h1>`
-    if (dadosObject.media_type === 'image') {
-        div.innerHTML = `<img src="${dadosObject.url}">`
-    } 
-    else  {
-        div.innerHTML = `<iframe src="${dadosObject.url}"></iframe>`
-    }
-    div.innerHTML += `<p>${dadosObject.explanation}</p>`
-    if (dadosObject.copyright !== undefined) {
-        div2.innerHTML = `<h6> Credits: ${dadosObject.copyright}</h6>`
-    } 
-    
+  div1.innerHTML = `<h1>${dadosObject.title} (${dadosObject.date})</h1>`;
+  if (dadosObject.media_type === "image") {
+    div.innerHTML = `<img src="${dadosObject.url}">`;
+  } else {
+    div.innerHTML = `<iframe src="${dadosObject.url}"></iframe>`;
+  }
+  div.innerHTML += `<p>${dadosObject.explanation}</p>`;
+  if (dadosObject.copyright !== undefined) {
+    div2.innerHTML = `<h6> Credits: ${dadosObject.copyright}</h6>`;
+  }
 }
